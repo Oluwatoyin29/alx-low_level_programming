ba15d95c046f1a13b88491b41df21465a675034f
Oluwatoyin29 <Oluwatoyin29@gmail.com>
2023-08-15 13:13:44 -0700

    header

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+/**
+ * int _putchar - writes the character c to stdout
+ * void print_alphabet - prints alphabet and returns new line
+ * void print_alphabet_x10 - print alphabet 10 times
+ * int _islower - return 1 if c is lowercase, 0 otherwise
+ * int _isalpha - return 1 if c is upper or lowercase, 0 otherwise
+ * int print_sign - return 1 for positive, 0 for 0, and -1 for negative
+ * int _abs - computes absolute value of integer
+ * int print_last_digit - print last digit
+ * int add - add two ints, return sum
+ * void jack_bauer - prints all minutes of the day
+ * void times_table - prints times table up to 9
+ * void print_to_98 - prints all natural numbers to 98
+ * void print_times_table - print times table up to
+ **/
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int);
+void print_times_table(int n);
