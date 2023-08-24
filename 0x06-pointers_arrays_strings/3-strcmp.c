dceabe7f1e4eb5c12f09ddd149669cc102846a16
Oluwatoyin29 <Oluwatoyin29@gmail.com>
2023-08-24 16:34:33 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcmp - this function given compares two strings
+ * @s1:string
+ * @s2:strmp
+ * Return: Int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
+
